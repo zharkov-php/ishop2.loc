7be3bf27ed07f9c6c2257a66f4ace136f8704262
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-19 23:05:14 +0200

    Create Index.php, .htaccess(front), .htaccess(public), init.php, autoload || Создали Index.php, настроили все запросы на Index.php, создали Константы, подключили файл автозагрузки

---
+++ config/init.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A0297
+ * Date: 19.03.2018
+ * Time: 22:33
+ */
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("www", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/ishop/core');
+define("LIBS", ROOT . '/vendor/ishop/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'default');
+
+//http://ishop2.loc/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+
+//http://ishop2.loc/public/
+//вырезаем с конца index.php
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+
+
+//http://ishop2.loc/
+$app_path = str_replace('/public/', '', $app_path);
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+//include autoload
+require_once ROOT . '/vendor/autoload.php';
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A0297
+ * Date: 19.03.2018
+ * Time: 22:22
+ */
+ require_once dirname(__DIR__) . '/config/init.php';
