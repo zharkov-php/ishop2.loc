6cbb8c1a60526b9dd40b829bac612b00844156d0
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-21 18:43:56 +0200

    Create Controller.php, View.php, AppController.php | Базовый контроллер

---
+++ app/controllers/AppController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A0297
+ * Date: 21.03.2018
+ * Time: 18:14
+ */
+
+namespace app\controllers;
+
+
+use ishop\base\Controller;
+
+
+class AppController extends Controller
+{
+    public function indexAction(){
+        echo __METHOD__;
+    }
+
+}
