a9fc822a5325837a2570979d4923e91747fed6bd
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-21 17:04:23 +0200

    Update Router | Доделал маршрутизатор

---
+++ app/controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A0297
+ * Date: 21.03.2018
+ * Time: 16:53
+ */
+
+namespace app\controllers;
+
+
+class MainController
+{
+    public function indexAction(){
+     echo __METHOD__;
+}
+}
