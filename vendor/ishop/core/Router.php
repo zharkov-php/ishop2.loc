eef618dbf1ab3229394cdc7676b8f481038cdc68
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-21 21:10:24 +0200

    Create Model | Создал Модель и подключил БД

--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -80,4 +80,16 @@ class Router{
     }
 
 
+    protected static function removeQueryString($url){
+        if($url){
+            $params = explode('&', $url, 2);
+            if(false === strpos($params[0], '=')){
+                return rtrim($params[0], '/');
+            }else{
+                return '';
+            }
+        }
+    }
+
+
 }
