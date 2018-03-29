a3af28c8f9aafa8f0c6f6b66316b681a9f5fbc59
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-29 20:32:54 +0300

    Управление товарами. Добавление фильтров (опции товаров)ДОБАВЛЕННА КРАСНАЯ КНОПКА СБРОСА

--- public/adminlte/my.js
+++ public/adminlte/my.js
@@ -1,6 +1,3 @@
-//CKEDITOR.replace('editor1');
-$('#editor1') .ckeditor();
-
 $('.delete').click(function(){
     var res = confirm('Подтвердите действие');
     if(!res) return false;
@@ -13,4 +10,34 @@ $('.sidebar-menu a').each(function(){
         $(this).parent().addClass('active');
         $(this).closest('.treeview').addClass('active');
     }
+});
+
+// CKEDITOR.replace('editor1');
+$( '#editor1' ).ckeditor();
+
+$('#reset-filter').click(function(){
+    $('#filter input[type=radio]').prop('checked', false);
+    return false;
+});
+
+$(".select2").select2({
+    placeholder: "Начните вводить наименование товара",
+    //minimumInputLength: 2,
+    cache: true,
+    ajax: {
+        url: adminpath + "/product/related-product",
+        delay: 250,
+        dataType: 'json',
+        data: function (params) {
+            return {
+                q: params.term,
+                page: params.page
+            };
+        },
+        processResults: function (data, params) {
+            return {
+                results: data.items,
+            };
+        },
+    },
 });
