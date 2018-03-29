ab3b596292638a488aeeba61159b793104bba688
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-29 11:49:42 +0300

    В Админке вывод 4 виджетов(НОвые заказы, Товары, Зарегистрированные пользователи, категории)

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -5,7 +5,12 @@ namespace app\controllers\admin;
 class MainController extends AppController {
 
     public function indexAction(){
+        $countNewOrders = \R::count('order', "status = '0'");
+        $countUsers = \R::count('user');
+        $countProducts = \R::count('product');
+        $countCategories = \R::count('category');
         $this->setMeta('Панель управления');
+        $this->set(compact('countNewOrders', 'countCategories', 'countProducts', 'countUsers'));
     }
 
 }
