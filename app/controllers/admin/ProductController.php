d14a3e6df67f20654ee295c1d7bacdfd58f6257d
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-29 22:43:24 +0300

    В Админке  Управление товарами. Загрузка картинок в товаре

--- app/controllers/admin/ProductController.php
+++ app/controllers/admin/ProductController.php
@@ -4,6 +4,7 @@ namespace app\controllers\admin;
 
 use app\models\admin\Product;
 use app\models\AppModel;
+use ishop\App;
 use ishop\libs\Pagination;
 
 class ProductController extends AppController {
@@ -19,6 +20,21 @@ class ProductController extends AppController {
         $this->set(compact('products', 'pagination', 'count'));
     }
 
+    public function addImageAction(){
+        if(isset($_GET['upload'])){
+            if($_POST['name'] == 'single'){
+                $wmax = App::$app->getProperty('img_width');
+                $hmax = App::$app->getProperty('img_height');
+            }else{
+                $wmax = App::$app->getProperty('gallery_width');
+                $hmax = App::$app->getProperty('gallery_height');
+            }
+            $name = $_POST['name'];
+            $product = new Product();
+            $product->uploadImg($name, $wmax, $hmax);
+        }
+    }
+
     public function addAction(){
         if(!empty($_POST)){
             $product = new Product();
@@ -26,6 +42,7 @@ class ProductController extends AppController {
             $product->load($data);
             $product->attributes['status'] = $product->attributes['status'] ? '1' : '0';
             $product->attributes['hit'] = $product->attributes['hit'] ? '1' : '0';
+            $product->getImg();
 
             if(!$product->validate($data)){
                 $product->getErrors();
@@ -34,6 +51,7 @@ class ProductController extends AppController {
             }
 
             if($id = $product->save('product')){
+                $product->saveGallery($id);
                 $alias = AppModel::createAlias('product', 'alias', $data['title'], $id);
                 $p = \R::load('product', $id);
                 $p->alias = $alias;
